501d7f2c9404fc26dcbe347048193335cf964b81
madman <hrigar02@gmail.com>
2024-11-03 18:28:55 +0530

    nothing

--- app/database.py
+++ app/database.py
@@ -1,11 +1,22 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:madman02@localhost:5432/fastapi"
+load_dotenv()
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+print(DATABASE_URL)
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autoflush=False,autocommit=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
